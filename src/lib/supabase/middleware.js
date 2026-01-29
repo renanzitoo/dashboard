bf52dc1a19e550b07b0eaca2ece6261a04a470db
Renan Costa <renan.costa2098@gmail.com>
2026-01-29 13:50:15 -0300

    update about section

--- src/lib/supabase/middleware.js
+++ src/lib/supabase/middleware.js
@@ -39,7 +39,8 @@ export async function updateSession(request) {
     !user &&
     !request.nextUrl.pathname.startsWith('/login') &&
     !request.nextUrl.pathname.startsWith('/auth') &&
-    !request.nextUrl.pathname.startsWith('/api/webhook')
+    !request.nextUrl.pathname.startsWith('/api/webhook') &&
+    !request.nextUrl.pathname.startsWith('/sobre')
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone()
